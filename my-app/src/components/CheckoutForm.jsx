9ce90133cd0681b920f3f2d92fc8e2d793e3f273
Aun-Ali-12 <aunali29331@gmail.com>
2026-01-02 01:04:55 +0500

    cart functionality completed

--- my-app/src/components/CheckoutForm.jsx
+++ my-app/src/components/CheckoutForm.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCart } from "../Context/CartContext";
 import { supabase } from "../services/supabaseClient";
 
 function CheckoutForm() {
     const { cart } = useCart(); //useCart context
-    const [user, setUser] = useState({
+    const [userDetail, setUserDetail] = useState({
         name: '',
         email: '',
         phone: '',
         address: ''
     })
-    const total = cart.reduce((sum, item) => sum + item.price * item.qty, 0)
+    const total = cart.reduce((sum, item) => sum + Number(item.price) * Number(item.qty), 0)
 
     const handleChange = (e) => {
         const { name, value } = e.target
-        setUser(prev => ({
+        setUserDetail(prev => ({
             ...prev,
             [name]: value
         }))
@@ -23,11 +23,17 @@ function CheckoutForm() {
     //On click place order
     const handleCheckout = async () => {
         //order detail in object:
+
+        // get login user
+        const { data: { user } } = await supabase.auth.getUser()
+        const userId = user.id
+
         const order = {
-            customer_name: user.name,
-            customer_email: user.email,
-            phone: user.phone,
-            address: user.address,
+            user_id: userId,
+            customer_name: userDetail.name,
+            customer_email: userDetail.email,
+            phone: userDetail.phone,
+            address: userDetail.address,
             items: cart,
             total_price: total,
             status: "pending",
@@ -41,22 +47,27 @@ function CheckoutForm() {
 
             if (error) {
                 alert("Error while adding placing order.", error.message)
+                console.log(error.message);
+                // console.log(user);
+                console.log(order);
+
                 return
             }
             alert("Order placed successfully")
         }
         catch (err) {
-            alert("Error while placing order",)
+            alert("Error while placing order", err)
         }
     }
 
     useEffect(() => {
-        console.log(user);
-    }, [user])
+        console.log(userDetail);
+    }, [userDetail])
 
     return (
         <>
             <div>Checkout Form</div>
+
             <div>{
                 cart && cart.map((item, index) => (
                     <div key={index}>
@@ -65,14 +76,16 @@ function CheckoutForm() {
                     </div>
                 ))
             }
-            <p>Total: {total}rs</p>
+                <p>Total: {total}rs</p>
             </div>
+
             {/* user details  */}
+
             <div>
-                Enter your name: <input type="text" name="name" onChange={() => { handleChange }} /> <br />
-                Enter your email: <input type="email" name="email" onChange={() => { handleChange }} /> <br />
-                Enter your phone: <input type="number" name="phone" onChange={() => { handleChange }} /> <br />
-                Enter your address: <input type="text" name="address" onChange={() => { handleChange }} />
+                Enter your name: <input type="text" name="name" value={userDetail.name} onChange={handleChange} /> <br />
+                Enter your email: <input type="email" name="email" value={userDetail.email} onChange={handleChange} /> <br />
+                Enter your phone: <input type="number" name="phone" value={userDetail.phone} onChange={handleChange} /> <br />
+                Enter your address: <input type="text" name="address" value={userDetail.address} onChange={handleChange} />
                 <button onClick={() => { handleCheckout() }}>Checkout</button>
             </div>
         </>
